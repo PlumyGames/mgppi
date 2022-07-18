14d6d04a0fc55f7b6c712a7ddd1b1900a095e271
GitHub Action <action@github.com>
2022-07-18 12:33:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/liplum/mgppi/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/liplum/mgppi/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.liplum.mgppi.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.liplum.mgppi.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/liplum/mgppi/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liplum/mgppi/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liplum.mgppi.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liplum.mgppi.MyBundle
 
 class MyApplicationService {
 
